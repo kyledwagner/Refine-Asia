21a61502ef657db704e74d6cc4f4da87158d04a8
kyledwagner <kylewag@me.com>
2019-01-19 14:49:09 +0700

    Added new Prettier config

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const leftPad = require("left-pad"); // Require left pad
-const output = leftPad("Hello, World!", 15); // Define output
+const leftPad = require('left-pad'); // Require left pad
+const output = leftPad('Hello, World!', 15); // Define output
 
 // Send output to the console
 console.log(output);
+
+let person = {
+  name: 'Yoda',
+  designation: 'Jedi Master ',
+};
+
+function trainJedi(jediWarrion) {
+  if (jediWarrion.name === 'Yoda') {
+    console.log('No need! already trained');
+  }
+  console.log(`Training ${jediWarrion.name} complete`);
+}
+
+trainJedi(person);
+trainJedi({ name: 'Adeel', designation: 'padawan' });
